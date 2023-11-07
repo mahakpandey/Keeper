0d790eeb163260f1cba6283b0d2fb411df7721c2
mahakpandey <98814352+mahakpandey@users.noreply.github.com>
2023-11-07 20:00:46 +0530

    Add: edit functionality

--- src/components/Records.jsx
+++ src/components/Records.jsx
@@ -2,63 +2,132 @@ import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
-import PlaylistAddOutlinedIcon from '@mui/icons-material/PlaylistAddOutlined';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import PlaylistAddOutlinedIcon from "@mui/icons-material/PlaylistAddOutlined";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import EditNoteOutlinedIcon from "@mui/icons-material/EditNoteOutlined";
+import FileDownloadDoneOutlinedIcon from "@mui/icons-material/FileDownloadDoneOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 
 const Records = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [data, setData] = useState([]);
+  const [ modal , setModal] = useState({name: '', email: ''})
 
-    function addData (){
-        setData([...data , {name, email}]);
-        setName('');
-        setEmail('')
-    }
+  const [open, setOpen] = React.useState(false);
+  let userName, userEmail
 
-    const removeData = (index) => {
-        let arr = data;
-        arr.splice(index, 1);
-        setData([...arr])
-    }
+  const handleOpen = (index) =>{
+    const foundElement  = data[index];
+    setModal(foundElement) 
+   
+    console.log('userName', userName)
+    console.log('userEmail', userEmail)
+    setOpen(true);
+  }
+  const handleClose = () => setOpen(false);
+
+  function addData() {
+    setData([...data, { name, email }]);
+    setName("");
+    setEmail("");
+  }
+
+  const removeData = (index) => {
+    let arr = data;
+    arr.splice(index, 1);
+    setData([...arr]);
+  };
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    border: "none",
+    borderRadius: "24px",
+    width: "30vw",
+    minWidth: " 215px",
+    p: "30px 50px 45px",
+    display: "flex",
+    flexDirection: "column",
+  };
 
   return (
     <>
-     <div className="form">
-      <Stack direction="row" spacing={2}>
-        <TextField 
+      <div className="form">
+        <Stack direction="row" spacing={2}>
+          <TextField
             value={name}
-            onChange={(event)=> setName(event.target.value)}
-            id="outlined-basic" 
-            label="Name" 
-            variant="outlined" />
-        <TextField 
+            onChange={(event) => setName(event.target.value)}
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+          />
+          <TextField
             value={email}
-            onChange={(event)=> setEmail(event.target.value)}
-            id="outlined-basic" 
-            label="Email" 
-            variant="outlined" />
-        <Button onClick={addData} variant="outlined" color="success">
-          <PlaylistAddOutlinedIcon />
-        </Button>
-      </Stack>
-    </div>
+            onChange={(event) => setEmail(event.target.value)}
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+          />
+          <Button onClick={addData} variant="outlined" color="success">
+            <PlaylistAddOutlinedIcon />
+          </Button>
+        </Stack>
+      </div>
 
-    <div className="data">
-        {
-            data.map((e,i)=>{
-                return (
-                    <div className='list'>
-                        <h3>{e.name}</h3>
-                        <h3>{e.email}</h3>
-                        <Button onClick={()=> removeData(i)} style={{height: '30px'}} variant="outlined"  color="error"><DeleteOutlineIcon/></Button>
-                    </div>
-                )
-            })
-        }
-    </div>
+      <div className="data">
+        {data.map((e, i) => {
+          return (
+            <div key={i} className="list">
+              <h3>{e.name}</h3>
+              <h3>{e.email}</h3>
+              <div>
+                <Button
+                  onClick={() => handleOpen(i)}
+                  variant="outlined"
+                  color="secondary"
+                  style={{ marginRight: "15px" }}
+                >
+                  <EditNoteOutlinedIcon />
+                </Button>
+                <Button
+                  onClick={() => removeData(i)}
+                  variant="outlined"
+                  color="error"
+                >
+                  <DeleteOutlineIcon />
+                </Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <Modal
+        open={open}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <TextField value={modal.name} id="standard-basic" label="Name" variant="standard"/>
+          <TextField value={modal.email} id="standard-basic" label="Email" variant="standard" style={{marginTop: "20px"}}/>
+          <div style={{ marginTop: "60px" }}>
+            <Button variant="outlined" color="success" style={{marginRight: "20px"}}>
+              <FileDownloadDoneOutlinedIcon />
+            </Button>
+            <Button onClick={handleClose} variant="outlined" color="error">
+              <CloseOutlinedIcon />
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </>
-   
   );
 };
 
